884cbcf62980a06c82df219d1e849d4f17b4c183
janusalarsen <janusalarsen@gmail.com>
2018-06-07 15:21:18 +0200

    Master add initial iteration of functions storeTodos, displayTodos, addTodos, changeTodos, deleteTodos

---
+++ todo.js
@@ -0,0 +1,19 @@
+function storeTodos(item) {
+  var todos = [item];
+}
+
+function displayTodos {
+  console.log('My todos:', todos);
+}
+
+function addTodos {
+  push.todos();
+}
+
+function changeTodos(item, newItem) {
+  todos[item] = newItem;
+}
+
+function deleteTodos{
+  splice.todos();
+}
