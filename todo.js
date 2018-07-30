400aa0daf85cc3f8952b4942f63696709e9ff772
janusalarsen <janusalarsen@gmail.com>
2018-07-30 13:47:31 +0200

    master add message if array is empty. Nest for loop in else

--- todo.js
+++ todo.js
@@ -2,8 +2,12 @@ var todoList = {
   todos: [],
   displayTodos: function(){
     console.log('My todos:');
-    for (var i = 0; i < this.todos.length; i++) {
-      console.log(this.todos[i].todoText);
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      for (var i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText);
+      }
     }
   },
   addTodo: function(todoText){
