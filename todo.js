eaab0029e4525738942373554de695118c9472df
janusalarsen <janusalarsen@gmail.com>
2018-07-30 14:03:00 +0200

    master add '(x)' or '( )' in relation to whether or not the todo is completed

--- todo.js
+++ todo.js
@@ -6,7 +6,11 @@ var todoList = {
       console.log('Your todo list is empty!');
     } else {
       for (var i = 0; i < this.todos.length; i++) {
-        console.log(this.todos[i].todoText);
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
       }
     }
   },
