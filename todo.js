f909c9a9ddc08c0b86cffff20adbc30fd7636780
janusalarsen <janusalarsen@gmail.com>
2018-08-09 09:32:38 +0200

    master refactor methods for buttons into object

--- todo.js
+++ todo.js
@@ -66,3 +66,12 @@ displayTodosButton.addEventListener('click', function(){
 toggleAllButton.addEventListener('click', function(){
   todoList.toggleAll();
 })
+
+var handlers = {
+  displayTodos: function(){
+    todoList.displayTodos();
+  },
+  toggleAll: function(){
+    todoList.toggleAll();
+  }
+};
